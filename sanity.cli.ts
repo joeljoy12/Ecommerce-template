cacf75d259e8da167334f3ebaf35f65f485fa986
joeljoy12 <joeljoy876@gmail.com>
2025-10-18 21:40:16 +0530

    trigger redeploy

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,11 +1,9 @@
-/**
-* This configuration file lets you run `$ sanity [command]` in this folder
-* Go to https://www.sanity.io/docs/cli to learn more.
-**/
-import { defineCliConfig } from 'sanity/cli'
+import { createClient } from "@sanity/client";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 
-export default defineCliConfig({ api: { projectId, dataset } })
-
+export const client = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  apiVersion: "2025-01-01", // or your latest API date
+  useCdn: true,
+});
