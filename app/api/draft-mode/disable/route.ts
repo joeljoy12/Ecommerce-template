2ca5c0471218b7eef38b4e848cca9bde2d42d228
joeljoy12 <joeljoy876@gmail.com>
2025-11-11 00:15:26 +0530

    fix:fixed enable and disable error

--- app/api/draft-mode/disable/route.ts
+++ app/api/draft-mode/disable/route.ts
@@ -1,7 +1,7 @@
 import { draftMode } from "next/headers"
 import { NextResponse } from "next/server"
 
-export  function GET(request: Request) {
-  draftMode().disable()// or .enable()
+export async  function  GET(request: Request) {
+  (await draftMode()).disable() 
   return NextResponse.redirect(new URL("/", request.url))
 }
