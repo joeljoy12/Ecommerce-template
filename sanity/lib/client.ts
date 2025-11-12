ddac36951d19b1dc7489d8e86c9ec61c19414730
joeljoy12 <joeljoy876@gmail.com>
2025-11-12 12:18:52 +0530

    fix: solved live.ts error

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,16 +1,11 @@
-import { createClient } from 'next-sanity'
-
-import { apiVersion, dataset, projectId } from '../env'
-
-const token = process.env.SANITY_VIEWER_TOKEN;
+import { createClient } from "next-sanity"
+import { apiVersion, dataset, projectId } from "../env"
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
   useCdn: true,
-  stega: { studioUrl: '/studio' },
-  serverToken: token,
-  browserToken: token,
-   
+  token: process.env.SANITY_VIEWER_TOKEN,
+  stega: { studioUrl: "/studio" },
 })
